81e665b03f1735e49e904fa4e976d032d87093e4
Aquil Rodrigues <aquil.ee17@bmsce.ac.in>
2021-07-19 15:48:29 +0530

    small changes

--- my_dictionary.py
+++ my_dictionary.py
@@ -1,5 +1,3 @@
-import ast
-import string
 def search():
     my_dict = open("words.txt", "r")
     word = input("Enter the word: ")
@@ -15,8 +13,6 @@ def search():
     if flag==1:
         print("Word not available")
     my_dict.close()
-
-
 def add_word():
     my_dict = open("words.txt", "a")
     word = input("Enter the word: ")
@@ -32,9 +28,6 @@ def add_word():
         my_dict = open("words.txt", "a")
         my_dict.write(new)
         my_dict.close()
-
-    
-
 def word_check(check):
     my_dict = open("words.txt", "r")
     
@@ -45,13 +38,9 @@ def word_check(check):
         line = line.split(":")
        
         if check == line[0]:
-            return 1
-        
-
-
+            return 1       
 choice = input("Enter 1 if you want to search \n Enter 2 if you want to add word : ")
 if choice== "1":
     search()
 else:
     add_word()
-#change
